464e57cc6184b3c25b501568e230258ee8be0f5b
Hamed <mehrabi.hamed@outlook.com>
2023-03-28 13:59:43 +0100

    Add Flask app + gunicorn

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return "Testing for the CI CD, Hello world"
+
+
+if __name__ == "__main__":
+    app.run(port=8080)
