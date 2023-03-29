2b4595ca46d91b04568a0fc7640f92b3d6542e1e
Hamed <mehrabi.hamed@outlook.com>
2023-03-29 16:26:16 +0100

    __init__.py updated

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,5 @@
 from src.stage_00_data_loader import DataLoader
-from src.stage_01_data_validation import DataValidation
 from src.stage_02_data_preprocessing import DataPreprocessing
-from src.stage_03_model_training import ModelTraining
+from src.stage_03_model_training import ModelTrainer
 from src.stage_04_model_evaluation import ModelEvaluation
 from src.stage_05_model_deployment import ModelDeployment
